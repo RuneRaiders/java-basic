70a63ca22ff063d110182378247248c00f06988e
RuneRaiders <hyeonseoban47@gmail.com>
2025-04-03 22:01:09 +0900

    김영한의 실전 자바 - OCP(Open-Closed Principle)

--- src/poly/car1/CarMain1.java
+++ src/poly/car1/CarMain1.java
@@ -15,5 +15,10 @@ public class CarMain1 {
         driver.setCar(model3Car);
         driver.drive();
 
+        // 차량 변경 (model3 -> new)
+        NewCar newCar = new NewCar();
+        driver.setCar(newCar);
+        driver.drive();
+
     }
 }
